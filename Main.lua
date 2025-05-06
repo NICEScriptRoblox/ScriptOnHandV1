520c71508c75dda01b233a66da9fae66934416a7
NICEScriptRoblox <niicebotbot1@gmail.com>
2025-05-06 15:11:00 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -308,3 +308,39 @@ local Button = Items:CreateButton({
 	end
    end,
 })
+
+
+
+
+
+local Button = Items:CreateButton({
+   Name = "Pink Oak Log",
+   Callback = function()
+  	local Character = game:GetService("Players").LocalPlayer.Character
+  	local ItemsFolder = workspace:FindFirstChild("Items")
+
+  	if not Character then return end
+
+  	local DistanceInFront = 5
+ 	local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+
+  	if not HumanoidRootPart then return end
+			
+	  local ItemsOnly = {
+	    ['Pink Oak Log'] = true,
+	  }
+
+	local TargetPosition = HumanoidRootPart.CFrame * CFrame.new(0, 0, -DistanceInFront)
+
+	  if ItemsFolder then
+	    for _, Item in pairs(ItemsFolder:GetChildren()) do
+	      if Item:IsA("BasePart") or Item:IsA("Model") then
+	        if ItemsOnly[Item.Name] then
+	          Item:PivotTo(TargetPosition)
+	        end
+	      end
+	    end
+	  end
+	end
+   end,
+})
